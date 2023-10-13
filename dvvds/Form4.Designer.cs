6380f7b47e96d33ffa6fe493f3d6c9df7f8dbbd0
Daniil Tabolkin <dtalin2001@gmail.com>
2023-10-13 21:59:03 +0300

    DVvDS
    An application for converting numbers in binary notation to decimal notation

---
+++ dvvds/Form4.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace dvvds
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(253, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Перевод двоичной системы ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "в десятичную систему";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 168);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(16, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Перевести";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(214, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(13, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Число двоичной системы";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(13, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Число десятичной системы";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "dvvds";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
+
---
+++ dvvds/Form4.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//перевод двочной системы в десятичную систему
+//20.10.22
+namespace dvvds
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+            //задаёт цвет фона.
+            //BackColor = Color.//BlueViolet;
+        }
+        //кнопка закрытия
+        //
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        //кнопка перевода.
+        //основные расчёты и значения.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //задаём сум равную нулю.
+            //прим. 1011 = 2^0+2^1+2^2+2^3,
+            //2^1 не считается так как 0,
+            //это я всё к тому что эти двойки
+            //нужно будет суммировать и 
+            //переменная сум как раз для этого.
+            double sum = 0;
+            //задаётся строка ссс к которой
+            //приравниваем первый текстбокс.
+            string sss = textBox1.Text;
+            //задаём массив сh и закидываем 
+            //туда строку ссс со свойством Lenght -
+            //оно содержит количество элементов,
+            //которое может хранить массив.
+            //прим. пишем число 111111 в нём
+            //шесть символов и именно число 
+            //шесть и заносится в свойство Lenght.
+            char[] ch = new char[sss.Length];
+            //превращаем строку ссс в char.
+            ch = sss.ToCharArray();
+            try
+            {
+            }
+            //блокировка ошибки формата.
+            catch (FormatException)
+            {
+            }
+            //блокировка ошибки 
+            //с огромными числами.
+            catch (OverflowException)
+            {
+            }
+            //цикл for
+            //итак, задаём i равную нулю -
+            //ноль, то есть i не должна превышать
+            //заданного количества.
+            //прим. задали 111 - это 3 символа, 
+            //значит, i не будет больше 3, ни 
+            //ни 4, ни 5, ок?
+            for (int i = 0; i < sss.Length; i++)
+            {
+                //теперь if
+                //если в массиве с переменной i
+                //встречается единица,
+                //(прим. 1101 - тут их три)
+                //то...
+                if (ch[i] == '1')
+                {
+                    //...то он расчитывает сумму
+                    //которая изначально равна нулю.
+                    //Math - этот класс предоставляет 
+                    //константы и стат.методы для
+                    //многих-многих функций, а метод
+                    //Pow возводит в указанную степень
+                    //(в нашем случае i) указанное
+                    //число (в нашем случае 2).
+                    sum = sum + Math.Pow(2, i);
+                }
+                //а если не встречается единица
+                //(то есть встречается 0), то
+                //цикл уходит сюда.
+                else { }
+            }
+
+     /*       Array.Reverse(ch);
+
+            for (int i = 0; i < sss.Length; i++)
+            {
+                if (ch[i] == '1')
+                {
+                    sum = sum + Math.Pow(2, i);
+                }  
+                else { }
+      * Array.Clear(....);
+       */    
+            textBox2.Text = "" + sum.ToString();
+        }
+        //текстовый бокс 1.
+        //задаются только единицы и нули
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //задаётся строка с цифрами 0 и 1.            
+            string ss = "01";
+            //если в строке ss находятся символы,
+            //кроме нуля и единицы, 
+            //то они блокируются.
+            //А также если это не символы, 
+            //то 0 и 1 могут стираться.
+            if (!ss.Contains(e.KeyChar)&& e.KeyChar != Convert.ToChar(Keys.Back))
+            {
+                //значение true - значит,
+                //событие обработано.
+                e.Handled = true;
+            }
+        } 
+        //текстовый бокс 1.
+        //откликается на enter и esc.
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == Convert.ToChar(Keys.Enter))
+            {
+                button1_Click(button1, null);
+            }
+            if (e.KeyValue == Convert.ToChar(Keys.Escape))
+            {
+                button2_Click(button2, null);
+            }
+        }
+    }
+}
+
